d6502c3478641adec7e3cb4dedfee02c780f5fd4
Kevin <kast46.15@gmail.com>
2018-05-16 01:12:38 -0500

    Add files via upload

--- sistema_ecuaciones.cpp
+++ sistema_ecuaciones.cpp
@@ -3,48 +3,79 @@
 
 using namespace std;
 
-double E[4][5] escalona(double A[4][5]);
-double X[4] sust_regresiva(double A[4][5], b[4]);
-double X[4] met_gauss(double A[4][5], double b[4]);
-double E[4][5] escalona_piv(double A[4][5]);
+const int filas = 4;
+const int columnas = 4;
+double **escalona(double AE[filas][columnas+1]);
+double *sust_regresiva(double A[filas][columnas], double [filas]);
+double *met_gauss(double A[filas][columnas], double b[filas]);
+double **escalona_piv(double AE[filas][columnas+1]);
+double *met_gauss_piv(double A[filas][columnas], double b[filas]);
 
 int main()
 {
-	
+	double A[filas][columnas]= {
+		{2,3,4,-5},
+		{-3,4,-5,7},
+		{5,1,-1,6},
+		{7,9,-13,17}
+	};
+	double b[filas] = {7,2,5,21};
 
+	double *X = met_gauss_piv(A,b);
+	
+	for(int f=0;f!=filas;f++)
+	        cout << "X["<<f+1<<"] = "<<X[f] << '\n';
+	        
+	//----- Solo para verificar
+	double r[filas];
+	for(int i=0; i <=filas-1; i++){
+	  r[i] = 0;
+	  for(int j=0; j<= filas-1; j++){
+		r[i] += A[i][j]*X[j];
+	  }
+	}
+	cout << '\n';
+	for(int i=0;i<=filas-1;i++)
+		cout << "b["<<i+1<<"] = "<<r[i] << '\n';
+	//----- 
+	 
 	return 0;
 }
 
-double E[4][5] escalona(double A[4][5])
+double **escalona(double AE[filas][columnas+1])
 {
-	int f = 4, c= 5;
+	int f = filas, c= columnas+1;
 	double m[f][c];
 	for(int i=0; i != f; i++){
 		for(int j=0; j != c; j++){
 			m[i][j] = 0;		
 		}	
 	}
-	for(int j=0; j != c-1; j++){
-		for(int j=j+1; j != f; j++){
-			m[i][j] = A[i][j]/A[j][j];
-			for(int k=0; k!= c; k++){
-				A[i][k] = A[i][k] - m[i][j]*A[j][k];
+	for(int j=0; j <= c-2; j++){
+		for(int i=j+1; i <= f-1; i++){
+			m[i][j] = AE[i][j]/AE[j][j];
+			for(int k=0; k<= c-1; k++){
+				AE[i][k] = AE[i][k] - m[i][j]*AE[j][k];
 			}
 		}	
 	}
-	double E[4][5];
+	
+	double **E = new double*[filas];
+    for(int i = 0; i < filas; i++)
+        E[i] = new double[columnas+1];
+	
 	for(int i=0; i != f; i++){
 		for(int j=0; j != c; j++){
-			E[i][j] = A[i][j];		
+			E[i][j] = AE[i][j];		
 		}	
 	}
 	return E;
 }
 
-double X[4] sust_regresiva(double A[4][5],double b[4])
+double *sust_regresiva(double A[filas][columnas], double b[filas])
 {
-	int f = 4;
-	double X[f];
+	int f = filas;
+	double *X = new double[f];
 	for(int i=0; i != f; i++){
 		X[i] = 0;
 	}
@@ -58,9 +89,9 @@ double X[4] sust_regresiva(double A[4][5],double b[4])
 	return X;
 }
 
-double X[4] met_gauss(double A[4][5], double b[4])
+double *met_gauss(double A[filas][columnas], double b[filas])
 {
-	int f = 4, c = 4;
+	const int f = filas, c = columnas;
 	double B[f][c+1];
 	for(int i=0; i != f; i++){
 		for(int j=0; j != c; j++){
@@ -68,60 +99,99 @@ double X[4] met_gauss(double A[4][5], double b[4])
 		}	
 	}
 	for(int i=0; i != f; i++){ //matriz extendida
-		B[i][4] = b[i];
+		B[i][c] = b[i];
 	}
-	B =  escalona(B[4][5]);
+	double **B_; 
+	B_ =  escalona(B);
 	double X[f];
 	double Ba[f][c];
 	double Bb[f];
 	for(int i=0; i != f; i++){
 		for(int j=0; j != c; j++){
-			Ba[i][j] = B[i][j];		
+			Ba[i][j] = B_[i][j];		
 		}	
 	}
 	for(int i=0; i != f; i++){ //matriz extendida
-		Bb[i] = B[i][4];
+		Bb[i] = B[i][columnas];
 	}
-	X = sust_regresiva(Ba,Bb);
-	return X;
+	double *X_ = sust_regresiva(Ba,Bb);
+	return X_;
 }	
 
-double A[4][5] pivotea(double A[4][5], int c);
-double E[4][5] escalona_piv(double A[4][5])
+void pivotea(double AE[filas][columnas+1], int c)
 {
-	int f = 4, c= 5;
+	// en la matriz extendida pivotea la columna c a partir de c, en c va el mayor elemento
+	int fila_mayor = c;
+	for(int f=c; f <= filas-1;f++){
+	    if(abs(AE[f][c]) >= abs(AE[fila_mayor][c]))
+	        fila_mayor = f;
+	}
+	double aux;
+	for(int col=0; col <= columnas;col++){
+	    aux = AE[c][col];
+	    AE[c][col] =  AE[fila_mayor][col]; 
+	    AE[fila_mayor][col] = aux;
+	}
+}
+double **escalona_piv(double AE[filas][columnas+1])
+{
+	int f = filas, c= columnas+1;
 	double m[f][c];
 	for(int i=0; i != f; i++){
 		for(int j=0; j != c; j++){
 			m[i][j] = 0;		
 		}	
 	}
-	for(int j=0; j != c-1; j++){
-		//pivoteamos
-		A = pivotea(A,j);
-		for(int j=j+1; j != f; j++){
-			m[i][j] = A[i][j]/A[j][j];
-			for(int k=0; k!= c; k++){
-				A[i][k] = A[i][k] - m[i][j]*A[j][k];
+	for(int j=0; j != c-2; j++){
+		pivotea(AE,j);
+		for(int i=j+1; i <= f-1; i++){		    	
+			m[i][j] = AE[i][j]/AE[j][j];
+			for(int k=0; k<= c-1; k++){
+				AE[i][k] = AE[i][k] - m[i][j]*AE[j][k];
 			}
 		}	
 	}
-	double E[4][5];
+	
+	double **E = new double*[filas];
+    for(int i = 0; i < filas; i++)
+        E[i] = new double[columnas+1];
+        
 	for(int i=0; i != f; i++){
 		for(int j=0; j != c; j++){
-			E[i][j] = A[i][j];		
+			E[i][j] = AE[i][j];		
 		}	
 	}
 	return E;
 }
 
-double A[4][5] pivotea(double A[4][5], int c)
+double *met_gauss_piv(double A[filas][columnas], double b[filas])
 {
-	// en la matriz extendida pivotea la columna c a partir de c+1
-	
+	const int f = filas, c = columnas;
+	double B[f][c+1];
+	for(int i=0; i != f; i++){
+		for(int j=0; j != c; j++){
+			B[i][j] = A[i][j];		
+		}	
+	}
+	for(int i=0; i != f; i++){ //matriz extendida
+		B[i][c] = b[i];
+	}
+	double **B_; 
+	B_ =  escalona_piv(B);
+	double X[f];
+	double Ba[f][c];
+	double Bb[f];
+	for(int i=0; i != f; i++){
+		for(int j=0; j != c; j++){
+			Ba[i][j] = B_[i][j];		
+		}	
+	}
+	for(int i=0; i != f; i++){ //matriz extendida
+		Bb[i] = B[i][columnas];
+	}
+	double *X_ = sust_regresiva(Ba,Bb);
+	return X_;
 }
-
-
 
 
 
